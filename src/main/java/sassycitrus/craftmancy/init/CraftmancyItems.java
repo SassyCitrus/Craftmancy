29fcdbbb487cefd26d99a0ab6daae576a79dc7bf
Tryton Van Meer <trytonvanmeer@gmail.com>
2019-06-15 07:12:00 -0400

    Added Edelilian Apple

--- src/main/java/sassycitrus/craftmancy/init/CraftmancyItems.java
+++ src/main/java/sassycitrus/craftmancy/init/CraftmancyItems.java
@@ -3,20 +3,24 @@ package sassycitrus.craftmancy.init;
 import net.minecraft.item.Item;
 import net.minecraftforge.registries.IForgeRegistry;
 import sassycitrus.craftmancy.item.ItemBase;
+import sassycitrus.craftmancy.item.ItemFoodAppleEdelila;
 
 public class CraftmancyItems
 {
     public static ItemBase gem_edelila = new ItemBase("gem_edelila");
+    public static ItemFoodAppleEdelila apple_edelila = new ItemFoodAppleEdelila();
 
     public static void register(IForgeRegistry<Item> registry)
     {
         registry.registerAll(
-            gem_edelila
+            gem_edelila,
+            apple_edelila
         );
     }
 
     public static void registerModels()
     {
         gem_edelila.registerItemModel();
+        apple_edelila.registerItemModel();
     }
 }
---
+++ src/main/java/sassycitrus/craftmancy/item/ItemFoodAppleEdelila.java
@@ -0,0 +1,63 @@
+package sassycitrus.craftmancy.item;
+
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.MobEffects;
+import net.minecraft.item.EnumRarity;
+import net.minecraft.item.ItemFood;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.world.World;
+import sassycitrus.craftmancy.Craftmancy;
+import sassycitrus.craftmancy.util.StringUtil;
+
+public class ItemFoodAppleEdelila extends ItemFood
+{
+    private static final String NAME = "apple_edelila";
+
+    public ItemFoodAppleEdelila()
+    {
+        super(4, 1.2F, false);
+        setUnlocalizedName(NAME);
+        setRegistryName(NAME);
+        setCreativeTab(Craftmancy.creativeTab);
+        setAlwaysEdible();
+    }
+
+    public void registerItemModel()
+    {
+        Craftmancy.proxy.registerItemRenderer(this, 0, NAME);
+    }
+
+    @Override
+    protected void onFoodEaten(ItemStack stack, World world, EntityPlayer player)
+    {
+        if (!world.isRemote)
+        {
+            player.addPotionEffect(new PotionEffect(MobEffects.REGENERATION, 600, 1));
+        }
+    }
+    
+    @Override
+    public EnumRarity getRarity(ItemStack stack)
+    {
+        return EnumRarity.RARE;
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World world, List<String> tooltip, ITooltipFlag flag)
+    {
+        if (StringUtil.isShiftKeyDown())
+        {
+            tooltip.add(StringUtil.getInfoText("info.entropycraft." + NAME));
+        }
+        else
+        {
+            tooltip.add(StringUtil.shiftForDetails());
+        }
+    }
+}
