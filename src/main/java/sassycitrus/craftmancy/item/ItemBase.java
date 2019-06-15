574e27ff3037427d5dbe819fcc2f25151016c99c
Tryton Van Meer <trytonvanmeer@gmail.com>
2019-06-14 22:45:04 -0400

    Added ItemBase

---
+++ src/main/java/sassycitrus/craftmancy/item/ItemBase.java
@@ -0,0 +1,58 @@
+package sassycitrus.craftmancy.item;
+
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.item.EnumRarity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import sassycitrus.craftmancy.Craftmancy;
+import sassycitrus.craftmancy.util.StringUtil;
+
+public class ItemBase extends Item
+{
+    protected EnumRarity rarity = EnumRarity.COMMON;
+    protected String name;
+
+    public ItemBase(String name)
+    {
+        this.name = name;
+
+        setUnlocalizedName(name);
+        setRegistryName(name);
+        setCreativeTab(Craftmancy.creativeTab);
+    }
+
+    public ItemBase setRarity(EnumRarity rarity)
+    {
+        this.rarity = rarity;
+        return this;
+    }
+
+    public void registerItemModel()
+    {
+        Craftmancy.proxy.registerItemRenderer(this, 0, name);
+    }
+
+    @Override
+    public EnumRarity getRarity(ItemStack stack)
+    {
+        return this.rarity;
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World world, List<String> tooltip, ITooltipFlag flag)
+    {
+        if (StringUtil.isShiftKeyDown())
+        {
+            tooltip.add(StringUtil.getInfoText("info.entropycraft." + this.name));
+        }
+        else
+        {
+            tooltip.add(StringUtil.shiftForDetails());
+        }   
+    }
+}
--- src/main/java/sassycitrus/craftmancy/proxy/ClientProxy.java
+++ src/main/java/sassycitrus/craftmancy/proxy/ClientProxy.java
@@ -1,18 +1,16 @@
 package sassycitrus.craftmancy.proxy;
 
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
+import net.minecraftforge.client.model.ModelLoader;
+import sassycitrus.craftmancy.Craftmancy;
 
 public class ClientProxy extends CommonProxy
 {
     @Override
     public void registerItemRenderer(Item item, int meta, String id)
     {
-
-    }
-
-    @Override
-    public void registerRenderers()
-    {
-        
+        ModelResourceLocation model = new ModelResourceLocation(Craftmancy.modid + ":" + id, "inventory");
+        ModelLoader.setCustomModelResourceLocation(item, meta, model);
     }
 }
