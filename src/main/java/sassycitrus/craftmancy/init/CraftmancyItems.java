e7d64008a52e56e0edfd99307340a9561f366a50
Tryton Van Meer <trytonvanmeer@gmail.com>
2019-06-15 00:05:35 -0400

    Added Edelila gem.

--- src/main/java/sassycitrus/craftmancy/init/CraftmancyItems.java
+++ src/main/java/sassycitrus/craftmancy/init/CraftmancyItems.java
@@ -6,15 +6,17 @@ import sassycitrus.craftmancy.item.ItemBase;
 
 public class CraftmancyItems
 {
+    public static ItemBase gem_edelila = new ItemBase("gem_edelila");
+
     public static void register(IForgeRegistry<Item> registry)
     {
         registry.registerAll(
-            
+            gem_edelila
         );
     }
 
     public static void registerModels()
     {
-        
+        gem_edelila.registerItemModel();
     }
 }
