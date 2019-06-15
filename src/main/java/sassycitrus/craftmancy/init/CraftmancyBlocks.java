9f82455363a09323900e5f80a56ee79712f20642
Tryton Van Meer <trytonvanmeer@gmail.com>
2019-06-15 00:04:59 -0400

    Added init classed for blocks/items.

---
+++ src/main/java/sassycitrus/craftmancy/init/CraftmancyBlocks.java
@@ -0,0 +1,27 @@
+package sassycitrus.craftmancy.init;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraftforge.registries.IForgeRegistry;
+
+public class CraftmancyBlocks
+{
+    public static void register(IForgeRegistry<Block> registry)
+    {
+        registry.registerAll(
+            
+        );
+    }
+
+    public static void registerItemBlocks(IForgeRegistry<Item> registry)
+    {
+        registry.registerAll(
+            
+        );
+    }
+
+    public static void registerModels()
+    {
+        
+    }
+}
---
+++ src/main/java/sassycitrus/craftmancy/init/CraftmancyItems.java
@@ -0,0 +1,20 @@
+package sassycitrus.craftmancy.init;
+
+import net.minecraft.item.Item;
+import net.minecraftforge.registries.IForgeRegistry;
+import sassycitrus.craftmancy.item.ItemBase;
+
+public class CraftmancyItems
+{
+    public static void register(IForgeRegistry<Item> registry)
+    {
+        registry.registerAll(
+            
+        );
+    }
+
+    public static void registerModels()
+    {
+        
+    }
+}
