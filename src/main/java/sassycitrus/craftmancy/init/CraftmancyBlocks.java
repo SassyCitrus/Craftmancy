f32f74b6d6be2a714d600ad152c5e55871d3ea6b
Tryton Van Meer <trytonvanmeer@gmail.com>
2019-06-15 07:24:41 -0400

    Added Edelila Block

--- src/main/java/sassycitrus/craftmancy/init/CraftmancyBlocks.java
+++ src/main/java/sassycitrus/craftmancy/init/CraftmancyBlocks.java
@@ -1,27 +1,31 @@
 package sassycitrus.craftmancy.init;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 import net.minecraft.item.Item;
 import net.minecraftforge.registries.IForgeRegistry;
+import sassycitrus.craftmancy.block.BlockBase;
 
 public class CraftmancyBlocks
 {
+    public static BlockBase blockEdelila = new BlockBase(Material.IRON, "edelila_block").setHardness(3.0F).setResistance(5.0F).setHarvestLevelBase("pickaxe", 2);
+
     public static void register(IForgeRegistry<Block> registry)
     {
         registry.registerAll(
-            
+            blockEdelila
         );
     }
 
     public static void registerItemBlocks(IForgeRegistry<Item> registry)
     {
         registry.registerAll(
-            
+            blockEdelila.createItemBlock()
         );
     }
 
     public static void registerModels()
     {
-        
+        blockEdelila.registerItemModel(Item.getItemFromBlock(blockEdelila));
     }
 }
