3e2b3078a0adb23c3a37713a8c3ee1df86d582be
Tryton Van Meer <trytonvanmeer@gmail.com>
2019-06-17 07:44:41 -0400

    Added wand varients

--- src/main/java/sassycitrus/craftmancy/init/CraftmancyItems.java
+++ src/main/java/sassycitrus/craftmancy/init/CraftmancyItems.java
@@ -10,14 +10,21 @@ public class CraftmancyItems
 {
     public static ItemBase GEM_EDELILA = new ItemBase("gem_edelila");
     public static FoodAppleEdelila EDELILA_APPLE = new FoodAppleEdelila();
-    public static Wand WAND = new Wand();
+
+    public static Wand WAND_0 = new Wand(0);
+    public static Wand WAND_1 = new Wand(1);
+    public static Wand WAND_2 = new Wand(2);
+    public static Wand WAND_3 = new Wand(3);
 
     public static void register(IForgeRegistry<Item> registry)
     {
         registry.registerAll(
             GEM_EDELILA,
             EDELILA_APPLE,
-            WAND
+            WAND_0,
+            WAND_1,
+            WAND_2,
+            WAND_3
         );
     }
 
@@ -25,6 +32,9 @@ public class CraftmancyItems
     {
         GEM_EDELILA.registerItemModel();
         EDELILA_APPLE.registerItemModel();
-        WAND.registerItemModel();
+        WAND_0.registerItemModel();
+        WAND_1.registerItemModel();
+        WAND_2.registerItemModel();
+        WAND_3.registerItemModel();
     }
 }
