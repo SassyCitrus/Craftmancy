1cd93d62198ef7eaef1cc94b22329dbd5fd69759
Tryton Van Meer <trytonvanmeer@gmail.com>
2019-06-21 08:35:00 -0400

    Return default wand for creative tab

--- src/main/java/sassycitrus/craftmancy/item/tool/Wand.java
+++ src/main/java/sassycitrus/craftmancy/item/tool/Wand.java
@@ -1,6 +1,10 @@
 package sassycitrus.craftmancy.item.tool;
 
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.EnumRarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.NonNullList;
 import sassycitrus.craftmancy.item.ItemBase;
 
 public class Wand extends ItemBase
@@ -11,5 +15,22 @@ public class Wand extends ItemBase
 
         setMaxStackSize(1);
         setRarity(EnumRarity.RARE);
+    }
+
+    @Override
+    public void getSubItems(CreativeTabs tab, NonNullList<ItemStack> items)
+    {
+        if (this.isInCreativeTab(tab))
+        {
+            ItemStack wand = new ItemStack(this);
+            NBTTagCompound nbt = new NBTTagCompound();
+
+            nbt.setString("core", "core:wood");
+            nbt.setString("binding", "binding:iron");
+            nbt.setString("gem", "gem:diamond");
+
+            wand.setTagCompound(nbt);
+            items.add(wand);
+        }
     }
 }
