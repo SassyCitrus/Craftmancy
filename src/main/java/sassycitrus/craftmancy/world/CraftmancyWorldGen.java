6f8d809393a3d642ecf3e51347f402cf16d67775
Tryton Van Meer <trytonvanmeer@gmail.com>
2019-06-21 16:00:44 -0400

    generateOre takes Predicate for WorldGenMinable and added OreGen for Mithril, and Feuermin in Nether

--- src/main/java/sassycitrus/craftmancy/world/CraftmancyWorldGen.java
+++ src/main/java/sassycitrus/craftmancy/world/CraftmancyWorldGen.java
@@ -2,7 +2,11 @@ package sassycitrus.craftmancy.world;
 
 import java.util.Random;
 
+import com.google.common.base.Predicate;
+
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.block.state.pattern.BlockMatcher;
+import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
@@ -20,14 +24,24 @@ public class CraftmancyWorldGen implements IWorldGenerator
         {
             generateOverworld(random, chunkX, chunkZ, world, generator, provider);
         }
+        else if (world.provider.getDimension() == -1)
+        {
+            generateNether(random, chunkX, chunkZ, world, generator, provider);
+        }
     }
 
     private void generateOverworld(Random random, int chunkX, int chunkZ, World world, IChunkGenerator generator, IChunkProvider provider)
     {
         generateOre(CraftmancyBlocks.ORE_EDELILA.getDefaultState(), world, random, chunkX * 16, chunkZ * 16, 4, 32, 1 + random.nextInt(5), 8);
+        generateOre(CraftmancyBlocks.ORE_MITHRIL.getDefaultState(), world, random, chunkX * 16, chunkZ * 16, 1, 63, 3 + random.nextInt(3), 12);
     }
 
-    private void generateOre(IBlockState ore, World world, Random random, int x, int z, int minY, int maxY, int size, int chances)
+    private void generateNether(Random random, int chunkX, int chunkZ, World world, IChunkGenerator generator, IChunkProvider provider)
+    {
+        generateOre(CraftmancyBlocks.ORE_FEUERMIN.getDefaultState(), world, random, chunkX * 16, chunkZ * 16, 32, 95, 2 + random.nextInt(4), 12, BlockMatcher.forBlock(Blocks.NETHERRACK));
+    }
+
+    private void generateOre(IBlockState ore, World world, Random random, int x, int z, int minY, int maxY, int size, int chances, Predicate<IBlockState> predicate)
     {
         int deltaY = maxY - minY;
 
@@ -39,8 +53,13 @@ public class CraftmancyWorldGen implements IWorldGenerator
                 z + random.nextInt(16)
             );
 
-            WorldGenMinable generator = new WorldGenMinable(ore, size);
+            WorldGenMinable generator = new WorldGenMinable(ore, size, predicate);
             generator.generate(world, random, pos);
         }
+    }
+
+    private void generateOre(IBlockState ore, World world, Random random, int x, int z, int minY, int maxY, int size, int chances)
+    {
+        generateOre(ore, world, random, x, z, minY, maxY, size, chances, BlockMatcher.forBlock(Blocks.STONE));
     }
 }
