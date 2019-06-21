86309c8a73e8f92797a217ab8474fb44c7f68eab
Tryton Van Meer <trytonvanmeer@gmail.com>
2019-06-21 08:34:26 -0400

    Register other parts

--- src/main/java/sassycitrus/craftmancy/init/CraftmancyWandParts.java
+++ src/main/java/sassycitrus/craftmancy/init/CraftmancyWandParts.java
@@ -11,7 +11,13 @@ public class CraftmancyWandParts
         WandPartRegistry registry = WandPartRegistry.getInstance();
         
         registry.registerWandPart("iron", PartType.BINDING, Items.IRON_INGOT);
+        registry.registerWandPart("gold", PartType.BINDING, Items.GOLD_INGOT);
+        registry.registerWandPart("mithril", PartType.BINDING, CraftmancyItems.INGOT_MITHRIL);
+
         registry.registerWandPart("wood", PartType.CORE, Items.STICK);
+
         registry.registerWandPart("diamond", PartType.GEM, Items.DIAMOND);
+        registry.registerWandPart("emerald", PartType.GEM, Items.EMERALD);
+        registry.registerWandPart("edelila", PartType.GEM, CraftmancyItems.GEM_EDELILA);
     }
 }
