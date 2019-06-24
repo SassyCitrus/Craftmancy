00eb7e25d2fa13abcba7819520d4712561894f0a
Tryton Van Meer <trytonvanmeer@gmail.com>
2019-06-24 16:07:08 -0400

    Added ArcaneTable

---
+++ src/main/java/sassycitrus/craftmancy/block/ArcaneTable.java
@@ -0,0 +1,34 @@
+package sassycitrus.craftmancy.block;
+
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.world.World;
+
+public class ArcaneTable extends BlockTileEntityBase<TileEntity>
+{
+    public ArcaneTable()
+    {
+        super(Material.WOOD, "arcane_table");
+        setHardness(2.5F);
+    }
+
+    @Override
+    public TileEntity createTileEntity(World world, IBlockState state)
+    {
+        return null;
+    }
+
+    @Override
+    public boolean isOpaqueCube(IBlockState state)
+    {
+        return false;
+    }
+
+    @Override
+    public BlockRenderLayer getBlockLayer()
+    {
+        return BlockRenderLayer.CUTOUT;
+    }
+}
