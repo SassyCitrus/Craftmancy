e827763b46fd7d459d7aab9ec6a46e59531ece2d
Tryton Van Meer <trytonvanmeer@gmail.com>
2019-07-08 23:05:24 -0400

    Updated mana hud texture

--- src/main/java/sassycitrus/craftmancy/gui/HUDHandler.java
+++ src/main/java/sassycitrus/craftmancy/gui/HUDHandler.java
@@ -3,7 +3,6 @@ package sassycitrus.craftmancy.gui;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.ScaledResolution;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
