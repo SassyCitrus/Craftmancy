064c90ac9c1439f3ffd198cfb6680a7eb761d971
Tryton Van Meer <trytonvanmeer@gmail.com>
2019-07-10 14:57:34 -0400

    Added Alterbaum blocks and soundtypes for other blocks

--- src/main/java/sassycitrus/craftmancy/init/CraftmancyBlocks.java
+++ src/main/java/sassycitrus/craftmancy/init/CraftmancyBlocks.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
@@ -11,6 +12,7 @@ import net.minecraftforge.registries.IForgeRegistry;
 import sassycitrus.craftmancy.Craftmancy;
 import sassycitrus.craftmancy.block.BlockArcaneTable;
 import sassycitrus.craftmancy.block.BlockBase;
+import sassycitrus.craftmancy.block.alterbaum.LogAlterbaum;
 import sassycitrus.craftmancy.block.ore.OreEdelila;
 import sassycitrus.craftmancy.block.ore.OreFeuermin;
 
@@ -18,13 +20,16 @@ public class CraftmancyBlocks
 {
     public static HashMap<String, Block> BLOCKS = new HashMap<String, Block>();
 
-    public static Block EDELILA_BLOCK = registerBlock(new BlockBase(Material.IRON, "edelila_block").setHardness(3.0F).setResistance(5.0F).setHarvestLevelBase("pickaxe", 2));
-    public static Block MITHRIL_BLOCK = registerBlock(new BlockBase(Material.IRON, "mithril_block").setHardness(3.0F).setResistance(5.0F).setHarvestLevelBase("pickaxe", 2));
-    public static Block FEUERMIN_BLOCK = registerBlock(new BlockBase(Material.IRON, "feuermin_block").setHardness(3.0F).setResistance(5.0F).setHarvestLevelBase("pickaxe", 2));
-    public static Block FEUERSTEEL = registerBlock(new BlockBase(Material.IRON, "feuersteel_block").setHardness(3.0F).setResistance(5.0F).setHarvestLevelBase("pickaxe", 2));
+    public static Block EDELILA_BLOCK = registerBlock(new BlockBase(Material.IRON, "edelila_block").setHardness(3.0F).setResistance(5.0F).setHarvestLevelBase("pickaxe", 2).setSoundType(SoundType.METAL));
+    public static Block MITHRIL_BLOCK = registerBlock(new BlockBase(Material.IRON, "mithril_block").setHardness(3.0F).setResistance(5.0F).setHarvestLevelBase("pickaxe", 2).setSoundType(SoundType.METAL));
+    public static Block FEUERMIN_BLOCK = registerBlock(new BlockBase(Material.IRON, "feuermin_block").setHardness(3.0F).setResistance(5.0F).setHarvestLevelBase("pickaxe", 2).setSoundType(SoundType.METAL));
+    public static Block FEUERSTEEL = registerBlock(new BlockBase(Material.IRON, "feuersteel_block").setHardness(3.0F).setResistance(5.0F).setHarvestLevelBase("pickaxe", 2).setSoundType(SoundType.METAL));
     public static Block ORE_EDELILA = registerBlock(new OreEdelila());
-    public static Block ORE_MITHRIL = registerBlock(new BlockBase(Material.ROCK, "ore_mithril").setHardness(3.0F).setResistance(5.0F).setHarvestLevelBase("pickaxe", 2));
+    public static Block ORE_MITHRIL = registerBlock(new BlockBase(Material.ROCK, "ore_mithril").setHardness(3.0F).setResistance(5.0F).setHarvestLevelBase("pickaxe", 2).setSoundType(SoundType.STONE));
     public static Block ORE_FEUERMIN = registerBlock(new OreFeuermin());
+    public static Block ALTERBAUM_LOG = registerBlock(new LogAlterbaum());
+    public static Block ALTERBAUM_WOOD = registerBlock(new BlockBase(Material.WOOD, "alterbaum_wood").setHardness(2.0F).setHarvestLevelBase("axe", 2).setSoundType(SoundType.WOOD));
+    public static Block ALTERBAUM_PLANKS = registerBlock(new BlockBase(Material.WOOD, "alterbaum_planks").setHardness(2.0F).setHarvestLevelBase("axe", 2).setSoundType(SoundType.WOOD));
     public static Block ARCANE_TABLE = registerBlock(new BlockArcaneTable());
 
     private static Block registerBlock(Block block)
