0c0435ab5d5e0c9e1800e6e580dcf585849e29d4
Tryton Van Meer <trytonvanmeer@gmail.com>
2019-07-10 16:19:16 -0400

    Added Alterbaum leaves

---
+++ src/main/java/sassycitrus/craftmancy/block/alterbaum/LeavesAlterbaum.java
@@ -0,0 +1,110 @@
+package sassycitrus.craftmancy.block.alterbaum;
+
+import java.util.List;
+import java.util.Random;
+
+import net.minecraft.block.BlockLeaves;
+import net.minecraft.block.BlockPlanks.EnumType;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.NonNullList;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import sassycitrus.craftmancy.Craftmancy;
+import sassycitrus.craftmancy.init.CraftmancyBlocks;
+
+public class LeavesAlterbaum extends BlockLeaves
+{
+    protected static final String NAME = "alterbaum_leaves";
+
+    public LeavesAlterbaum()
+    {
+        setUnlocalizedName(NAME);
+        setRegistryName(NAME);
+        setCreativeTab(Craftmancy.creativeTab);
+        setHardness(0.2F);
+        setDefaultState(blockState.getBaseState().withProperty(CHECK_DECAY, Boolean.valueOf(true)).withProperty(DECAYABLE, Boolean.valueOf(true)));
+    }
+
+    @Override
+    public Item getItemDropped(IBlockState state, Random rand, int fortune)
+    {
+        return Item.getItemFromBlock(CraftmancyBlocks.ALTERBAUM_SAPLING);
+    }
+
+    @Override
+    protected ItemStack getSilkTouchDrop(IBlockState state)
+    {
+        return new ItemStack(Item.getItemFromBlock(this));
+    }
+
+    @Override
+    public IBlockState getStateFromMeta(int meta)
+    {
+        return getDefaultState().withProperty(DECAYABLE, Boolean.valueOf((meta & 4) == 0)).withProperty(CHECK_DECAY, Boolean.valueOf((meta & 8) > 0));
+    }
+
+    @Override
+    public int getMetaFromState(IBlockState state)
+    {
+        int i = 0;
+
+        if (!state.getValue(DECAYABLE).booleanValue())
+        {
+            i |= 4;
+        }
+
+        if (state.getValue(CHECK_DECAY).booleanValue())
+        {
+            i |= 8;
+        }
+
+        return i;
+    }
+
+    @Override
+    protected BlockStateContainer createBlockState()
+    {
+        return new BlockStateContainer(this, new IProperty[] {CHECK_DECAY, DECAYABLE});
+    }
+
+    @Override
+    public List<ItemStack> onSheared(ItemStack item, IBlockAccess world, BlockPos pos, int fortune)
+    {
+        return NonNullList.withSize(1, new ItemStack(this));
+    }
+
+    @Override
+    public EnumType getWoodType(int meta)
+    {
+        return null;
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public BlockRenderLayer getBlockLayer()
+    {
+        return Blocks.LEAVES.getBlockLayer();
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public boolean shouldSideBeRendered(IBlockState blockState, IBlockAccess blockAccess, BlockPos pos, EnumFacing side)
+    {
+        return Blocks.LEAVES.shouldSideBeRendered(blockState, blockAccess, pos, side);
+    }
+
+    @Override
+    public boolean isOpaqueCube(IBlockState state)
+    {
+        return Blocks.LEAVES.isOpaqueCube(state);
+    }
+}
