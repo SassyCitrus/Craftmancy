32b0e9c6e3f911a47f5c854b00537ad103fe0073
Tryton Van Meer <trytonvanmeer@gmail.com>
2019-07-10 06:05:12 -0400

    Moved FACING property to BlockBase

--- src/main/java/sassycitrus/craftmancy/block/BlockBase.java
+++ src/main/java/sassycitrus/craftmancy/block/BlockBase.java
@@ -2,10 +2,19 @@ package sassycitrus.craftmancy.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import sassycitrus.craftmancy.Craftmancy;
 
 public class BlockBase extends Block
 {
+    public static final PropertyDirection FACING = PropertyDirection.create("facing", EnumFacing.Plane.HORIZONTAL);
     protected String name;
 
     public BlockBase(Material material, String name)
@@ -17,6 +26,7 @@ public class BlockBase extends Block
         setUnlocalizedName(name);
         setRegistryName(name);
         setCreativeTab(Craftmancy.creativeTab);
+        setDefaultState(blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
     }
 
     public BlockBase setHardness(float hardness)
@@ -35,5 +45,34 @@ public class BlockBase extends Block
     {
         this.setHarvestLevel(tool, level);
         return this;
+    }
+
+    @Override
+    public void onBlockPlacedBy(World world, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack)
+    {
+        world.setBlockState(pos, state.withProperty(FACING, getFacingFromEntity(pos, placer)), 2);
+    }
+
+    public static EnumFacing getFacingFromEntity(BlockPos pos, EntityLivingBase entity)
+    {
+        return entity.getHorizontalFacing().getOpposite();
+    }
+
+    @Override
+    public IBlockState getStateFromMeta(int meta)
+    {
+        return getDefaultState()
+                .withProperty(FACING, EnumFacing.getHorizontal(meta));
+    }
+
+    @Override
+    public int getMetaFromState(IBlockState state)
+    {
+        return state.getValue(FACING).getHorizontalIndex();
+    }
+
+    @Override
+    protected BlockStateContainer createBlockState() {
+        return new BlockStateContainer(this, FACING);
     }
 }
--- src/main/java/sassycitrus/craftmancy/block/BlockTileEntityBase.java
+++ src/main/java/sassycitrus/craftmancy/block/BlockTileEntityBase.java
@@ -3,55 +3,17 @@ package sassycitrus.craftmancy.block;
 import javax.annotation.Nullable;
 
 import net.minecraft.block.material.Material;
-import net.minecraft.block.properties.PropertyDirection;
-import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 public abstract class BlockTileEntityBase<TE extends TileEntity> extends BlockBase
 {
-    public static final PropertyDirection FACING = PropertyDirection.create("facing", EnumFacing.Plane.HORIZONTAL);
-
     public BlockTileEntityBase(Material material, String name)
     {
         super(material, name);
-        setDefaultState(blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
-    }
-
-    @Override
-    public void onBlockPlacedBy(World world, BlockPos pos, IBlockState state, EntityLivingBase placer,
-            ItemStack stack)
-    {
-        world.setBlockState(pos, state.withProperty(FACING, getFacingFromEntity(pos, placer)), 2);
-    }
-
-    public static EnumFacing getFacingFromEntity(BlockPos pos, EntityLivingBase entity)
-    {
-        return entity.getHorizontalFacing().getOpposite();
-    }
-
-    @Override
-    public IBlockState getStateFromMeta(int meta)
-    {
-        return getDefaultState()
-                .withProperty(FACING, EnumFacing.getHorizontal(meta));
-    }
-
-    @Override
-    public int getMetaFromState(IBlockState state)
-    {
-        return state.getValue(FACING).getHorizontalIndex();
-    }
-
-    @Override
-    protected BlockStateContainer createBlockState() {
-        return new BlockStateContainer(this, FACING);
     }
 
     public abstract Class<TE> getTileEntityClass();
