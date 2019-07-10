6ea58cec53ed6852c7c2b9b3e01459ef018ece69
Tryton Van Meer <trytonvanmeer@gmail.com>
2019-07-10 11:50:26 -0400

    Added tooltip

--- src/main/java/sassycitrus/craftmancy/item/tool/Wand.java
+++ src/main/java/sassycitrus/craftmancy/item/tool/Wand.java
@@ -1,7 +1,13 @@
 package sassycitrus.craftmancy.item.tool;
 
+import java.util.List;
+
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.EnumRarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 import sassycitrus.craftmancy.item.ItemBase;
+import sassycitrus.craftmancy.util.StringUtil;
 
 public class Wand extends ItemBase
 {
@@ -11,5 +17,18 @@ public class Wand extends ItemBase
 
         setMaxStackSize(1);
         setRarity(EnumRarity.RARE);
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, World worldIn, List<String> tooltip, ITooltipFlag flagIn)
+    {
+        if (StringUtil.isShiftKeyDown())
+        {
+            tooltip.add(StringUtil.getInfoText("info.craftmancy." + name));
+        }
+        else
+        {
+            tooltip.add(StringUtil.shiftForDetails());
+        }
     }
 }
