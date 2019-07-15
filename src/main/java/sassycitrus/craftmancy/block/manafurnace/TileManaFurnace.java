9995bff6a95e4f34d58b61d86e9b069ff7f1d074
Tryton Van Meer <trytonvanmeer@gmail.com>
2019-07-15 10:58:23 -0400

    Added inventory capability

--- src/main/java/sassycitrus/craftmancy/block/manafurnace/TileManaFurnace.java
+++ src/main/java/sassycitrus/craftmancy/block/manafurnace/TileManaFurnace.java
@@ -1,18 +1,25 @@
 package sassycitrus.craftmancy.block.manafurnace;
 
+import javax.annotation.Nullable;
+
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.EnumFacing;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.ItemStackHandler;
 import sassycitrus.craftmancy.block.TileEntityBase;
 
 public class TileManaFurnace extends TileEntityBase
 {
+    private ItemStackHandler inventory = new ItemStackHandler(1);
+
     private boolean isActive = false;
 
     @Override
     public NBTTagCompound writeToNBT(NBTTagCompound nbt)
     {
         super.writeToNBT(nbt);
-        nbt.setBoolean("active", this.isActive);
-
+        nbt.setTag("inventory", inventory.serializeNBT());
         return nbt;
     }
 
@@ -20,17 +27,24 @@ public class TileManaFurnace extends TileEntityBase
     public void readFromNBT(NBTTagCompound nbt)
     {
         super.readFromNBT(nbt);
-        this.isActive = nbt.getBoolean("active");
+        inventory.deserializeNBT(nbt.getCompoundTag("inventory"));
+    }
+
+    @Override
+    public boolean hasCapability(Capability<?> capability, @Nullable EnumFacing facing)
+    {
+        return capability == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY || super.hasCapability(capability, facing);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T> T getCapability(Capability<T> capability, @Nullable EnumFacing facing)
+    {
+        return capability == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY ? (T) inventory : super.getCapability(capability, facing);
     }
 
     public boolean isActive()
     {
         return this.isActive;
-    }
-
-    public void rightClick()
-    {
-        this.isActive = !this.isActive;
-        sendUpdates();
     }
 }
