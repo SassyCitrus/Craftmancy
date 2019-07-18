7fc3d306d10942785a3767e61294769d62885f75
Tryton Van Meer <trytonvanmeer@gmail.com>
2019-07-18 17:31:18 -0400

    Updated Mana HUD
    Cancels drawing of experience bar when holding wand and draws mana bar in it's place. And now shows mana level.

--- src/main/java/sassycitrus/craftmancy/gui/HUDHandler.java
+++ src/main/java/sassycitrus/craftmancy/gui/HUDHandler.java
@@ -15,6 +15,7 @@ import sassycitrus.craftmancy.Craftmancy;
 import sassycitrus.craftmancy.capability.ManaCapabilityHandler;
 import sassycitrus.craftmancy.capability.ManaCapabilityHandler.IManaHandler;
 import sassycitrus.craftmancy.item.tool.Wand;
+import sassycitrus.craftmancy.util.ManaUtil;
 
 @Mod.EventBusSubscriber(value = Side.CLIENT, modid = Craftmancy.modid)
 @SideOnly(Side.CLIENT)
@@ -23,24 +24,25 @@ public class HUDHandler
     private static ResourceLocation MANA_BAR = new ResourceLocation("craftmancy:textures/gui/hud/mana_bar.png");
 
     @SubscribeEvent
-    public static void onDraw(RenderGameOverlayEvent.Post event)
+    public static void onDraw(RenderGameOverlayEvent.Pre event)
     {
         if (event.getType() == ElementType.EXPERIENCE)
         {
-            ScaledResolution resolution = event.getResolution();
-
-            drawManaBar(resolution);
+            drawManaBar(event);
         }
     }
 
-    private static void drawManaBar(ScaledResolution resolution)
+    private static void drawManaBar(RenderGameOverlayEvent.Pre event)
     {
         Minecraft mc = Minecraft.getMinecraft();
+        ScaledResolution resolution = event.getResolution();
 
         if (!(mc.player.getHeldItemMainhand().getItem() instanceof Wand))
         {
             return;
         }
+
+        event.setCanceled(true);
 
         IManaHandler player = mc.player.getCapability(ManaCapabilityHandler.CAPABILITY_MANA, EnumFacing.DOWN);
 
@@ -53,14 +55,24 @@ public class HUDHandler
         int height = 5;
 
         int x = resolution.getScaledWidth() / 2 - width / 2;
-        int y = resolution.getScaledHeight() - height - 40;
+        int y = resolution.getScaledHeight() - height - 24;
+        int manaBarWidth = (int) (player.getMana() / (float) ManaUtil.calculateManaForLevel(player.getManaLevel()) * width);
 
         mc.renderEngine.bindTexture(MANA_BAR);
 
         Gui.drawModalRectWithCustomSizedTexture(x, y, 0, 0, width, height, 192, 16);
-
-        int manaBarWidth = (int) (player.getMana() / (float) player.getCapacity() * width);
-
         Gui.drawModalRectWithCustomSizedTexture(x, y, 0, height, manaBarWidth, height, 192, 16);
+
+        if (player.getManaLevel() > 0)
+        {
+            String s = "" + player.getManaLevel();
+            int i1 = (resolution.getScaledWidth() - mc.fontRenderer.getStringWidth(s)) / 2;
+            int j1 = resolution.getScaledHeight() - 31 - 4;
+            mc.fontRenderer.drawString(s, i1 + 1, j1, 0);
+            mc.fontRenderer.drawString(s, i1 - 1, j1, 0);
+            mc.fontRenderer.drawString(s, i1, j1 + 1, 0);
+            mc.fontRenderer.drawString(s, i1, j1 - 1, 0);
+            mc.fontRenderer.drawString(s, i1, j1, 0xB79BEF);
+        }
     }
 }
