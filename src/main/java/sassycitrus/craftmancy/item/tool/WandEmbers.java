a5f6d65a30c6b05d20a58eaf26dfd9b4602beaa2
Tryton Van Meer <trytonvanmeer@gmail.com>
2019-07-22 23:26:50 -0400

    Wand of Embers now fires fireballs

--- src/main/java/sassycitrus/craftmancy/item/tool/WandEmbers.java
+++ src/main/java/sassycitrus/craftmancy/item/tool/WandEmbers.java
@@ -1,9 +1,32 @@
 package sassycitrus.craftmancy.item.tool;
 
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.SoundEvents;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.world.World;
+import sassycitrus.craftmancy.entity.EntityFireball;
+
 public class WandEmbers extends Wand
 {
     public WandEmbers()
     {
-        super("embers");   
+        super("embers");
+    }
+
+    @Override
+    public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand)
+    {
+        if (!world.isRemote)
+        {        
+            EntityFireball fireball = new EntityFireball(world, player);
+            world.spawnEntity(fireball);
+        }
+
+        player.swingArm(hand);
+        player.playSound(SoundEvents.ENTITY_BLAZE_SHOOT, 1, 1);
+        return new ActionResult<ItemStack>(EnumActionResult.PASS, player.getHeldItem(hand));
     }
 }
