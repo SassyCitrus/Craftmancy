2fee66779721958039a7c4555a88c0756280c429
Tryton Van Meer <trytonvanmeer@gmail.com>
2019-07-25 22:29:42 -0400

    Changed biome properties

--- src/main/java/sassycitrus/craftmancy/world/BiomeElderForest.java
+++ src/main/java/sassycitrus/craftmancy/world/BiomeElderForest.java
@@ -5,6 +5,8 @@ import java.util.Random;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.feature.WorldGenAbstractTree;
+import net.minecraft.world.gen.feature.WorldGenBirchTree;
+import net.minecraft.world.gen.feature.WorldGenTrees;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import sassycitrus.craftmancy.Craftmancy;
@@ -12,23 +14,26 @@ import sassycitrus.craftmancy.Craftmancy;
 public class BiomeElderForest extends Biome
 {
     public static final String NAME = "elder_forest";
-    private static final WorldGenTreeAlterbaum TREE_ALTERBAUM_FEATURE = new WorldGenTreeAlterbaum(false);
+    private static final WorldGenTreeAlterbaum FEATURE_TREE_ALTERBAUM = new WorldGenTreeAlterbaum(false);
+    private static final WorldGenBirchTree FEATURE_TREE_BIRCH = new WorldGenBirchTree(false, false);
+    private static final WorldGenTrees FEATURE_TREE_OAK = new WorldGenTrees(false);
 
     public BiomeElderForest()
     {
         super(new BiomeProperties(NAME)
                 .setTemperature(0.7F)
                 .setRainfall(0.8f)
-                .setBaseHeight(0.2F)
-                .setHeightVariation(0.3F)
+                .setBaseHeight(0.1F)
+                .setHeightVariation(0.05F)
                 .setWaterColor(0x925CFF)
         );
 
         this.decorator.sandPatchesPerChunk = 0;
         this.decorator.gravelPatchesPerChunk = 0;
-        this.decorator.grassPerChunk = 2;
-        this.decorator.treesPerChunk = 6;
+        this.decorator.grassPerChunk = 0;
+        this.decorator.treesPerChunk = 10;
         this.decorator.flowersPerChunk = 1;
+        this.decorator.mushroomsPerChunk = 0;
         this.decorator.generateFalls = false;
 
         setRegistryName(Craftmancy.modid, NAME);
@@ -39,7 +44,18 @@ public class BiomeElderForest extends Biome
     @Override
     public WorldGenAbstractTree getRandomTreeFeature(Random rand)
     {
-        return TREE_ALTERBAUM_FEATURE;
+        if (rand.nextInt(10) == 0)
+        {
+            return FEATURE_TREE_ALTERBAUM;
+        }
+        else if (rand.nextInt(5) == 0)
+        {
+            return FEATURE_TREE_BIRCH;
+        }
+        else
+        {
+            return FEATURE_TREE_OAK;
+        }
     }
 
     private void setSpawnables()
